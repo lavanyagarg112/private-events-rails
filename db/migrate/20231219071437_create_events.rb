2cac825ad6126bc0256b4424fa28780faef23c7a
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-19 15:16:52 +0800

    event model added

---
+++ db/migrate/20231219071437_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.date :date
+      t.string :location
+      t.references :creator, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
