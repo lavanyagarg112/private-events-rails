841cef6e1e7b811c534b21ac1a43425131f37762
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-19 16:42:16 +0800

    working on the database

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,25 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.created_events.build(event_params)
+        
+        if @event.save
+            redirect_to events_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:name, :date, :location)
+    end
+
 end
