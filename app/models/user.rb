a2d4f035284b12c0828acb07215ce5b4e739395a
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-19 15:54:05 +0800

    username added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+
+
 end
